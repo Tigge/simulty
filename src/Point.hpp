7b1905449b32ffe73ad3cce4aef27cbd3981cf9f
tigge <tigge@269fae90-12ad-11df-92fd-07e443e6c06b>
2007-05-18 12:19:48 +0000

    Added Doxyfile to generate documentation and updated documentation in Point.hpp

--- src/Point.hpp
+++ src/Point.hpp
@@ -7,7 +7,7 @@
 /** 
  * Simple class representation of a point. 
  * 
- * Etc.. more info
+ * Etc.. more info.
  *
  */
 class Point {
@@ -35,16 +35,59 @@ class Point {
      * @return the y value
      */    
     int getY();
-    
+
+    /** 
+     * Setter for the x value.
+     *
+     * @param x the new x value
+     */    
     void setX(int x);
+    
+    /** 
+     * Setter for the y value.
+     *
+     * @param y the new y value
+     */        
     void setY(int y);
 
+
+    /** 
+     * Translates (adds) the x and y value to the current point
+     *
+     * @param x the x value to add
+     * @param y the x value to add     
+     */    
     void translate(int x, int y);
+    /** 
+     * Translates (adds) the given point to the current point.
+     *
+     * This method just calls the translate(int x, int y) functions.
+     *
+     * @param p the point to add  
+     * @see     translate(int x, int y)
+     */        
     void translate(Point p);
-    
+
+    /** 
+     * Moves the point to a new x and y value
+     *
+     * @param x the new x value
+     * @param y the new y value  
+     */        
     void move(int x, int y);
+    /** 
+     * Moves the point to a new point
+     *
+     * @param p the point to move to
+     */       
     void move(Point p);
 
+    /** 
+     * Calculates distance from point a to point b
+     *
+     * @param a point a
+     * @param b point b
+     */   
     static int  distance(Point a, Point b);
     
     static void fixOrder(Point &a, Point &b);
