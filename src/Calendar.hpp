5287aa5ebaceb96e94e7b1478a20b8456de09e6e
tigge <tigge@269fae90-12ad-11df-92fd-07e443e6c06b>
2007-05-26 23:48:14 +0000

    Added calendar, forgot this one too

---
+++ src/Calendar.hpp
@@ -0,0 +1,82 @@
+
+#ifndef _CALENDAR_HPP_
+#define _CALENDAR_HPP_
+
+#include <string>
+
+/*
+#include <libintl.h>
+#include <gettext.h>
+#define _(string) gettext (string)
+*/
+#define _(string) string
+
+class Calendar {
+
+  private:
+
+  int year;
+  int month;
+  int day;
+
+  public:
+  
+  Calendar() {
+    year  = 1920;
+    month = 1;
+    day   = 1;
+  }
+  
+
+  int         getYear() {
+    return year;
+  }
+
+  int         getMonth() {
+    return month;
+  }
+  const std::string getMonthAsString() {
+    switch(getMonth()) {
+      case  1: return std::string(_("January"));
+      case  2: return std::string(_("February"));
+      case  3: return std::string(_("Mars"));
+      case  4: return std::string(_("April"));
+      case  5: return std::string(_("May"));
+      case  6: return std::string(_("Juni"));
+      case  7: return std::string(_("Juli"));
+      case  8: return std::string(_("August"));
+      case  9: return std::string(_("September"));
+      case 10: return std::string(_("October"));
+      case 11: return std::string(_("November"));
+      case 12: return std::string(_("December"));    
+    }  
+    
+    // TODO
+    return std::string("ERROR");
+  }
+
+  int         getDay() {
+    return day;
+  }
+
+  bool        isEndOfYear() {
+    return (day == 30 && month == 12);
+  }
+
+  void        advance() {
+    if(day == 30) {
+      day = 1;
+      if(month == 12) {
+        month = 1;
+        year++;
+      } else {
+        month++;
+      }
+    } else {
+      day++;
+    }
+  }
+};
+
+#endif
+
