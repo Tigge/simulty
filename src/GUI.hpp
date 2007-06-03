31802e346517cf0f31e48b4f27244692ac63020e
tigge <tigge@269fae90-12ad-11df-92fd-07e443e6c06b>
2007-06-03 18:53:12 +0000

    Added new base GUI.hpp file

---
+++ src/GUI.hpp
@@ -0,0 +1,26 @@
+
+
+#ifndef _GUI_H_
+#define _GUI_H_
+
+
+class Client;
+
+class GUI {
+  private:
+      
+  public:
+
+  virtual void console_log(std::string s) = 0;
+
+  GUI() {
+  
+  }
+
+  virtual ~GUI() {
+  
+  }
+
+};
+
+#endif
